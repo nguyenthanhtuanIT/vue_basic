8f33f691d4cd8a326a5620ebf5706be69a106ce5
ThanhTuanIT <nguyenthanhtuan.15it@gmail.com>
2020-09-11 23:43:09 +0700

    feat end game

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         playerHP: 100,
         monsterHP: 100,
-        isStart: false
+        isStart: false,
+        process: []
     },
     methods: {
         newGame: function() {
@@ -14,6 +15,7 @@ new Vue({
         attack: function() {
             if (this.monsterHP <= 0) {
                 alert("You win ^_^");
+                this.monsterHP = 0;
                 this.isStart = false;
                 return;
             }
@@ -22,9 +24,14 @@ new Vue({
             min = 5;
             damage = Math.max(Math.floor(Math.random() * max) + 1, min);
             this.monsterHP -= damage;
+            this.process.unshift({
+                isPlayer: true,
+                content: "Player hits monster for " + damage
+            });
 
             if (this.playerHP <= 0) {
                 alert("You lost ^_^");
+                this.playerHP = 0;
                 this.isStart = false;
                 return;
             }
@@ -33,8 +40,34 @@ new Vue({
             min = 5;
             damage = Math.max(Math.floor(Math.random() * max) + 1, min);
             this.playerHP -= damage;
-
-
+            this.process.unshift({
+                isPlayer: false,
+                content: "Monster hits player for " + damage
+            });
+        },
+        spAttack: function(max, min) {
+            if (this.monsterHP > this.playerHP) {
+                this.monsterHP -=  Math.max(Math.floor(Math.random() * max) + 1, min);
+                this.process.unshift({
+                    isPlayer: true,
+                    content: "Player hits monster for " + damage
+                });
+            }
+        },
+        heal: function() {
+            if (this.playerHP > 50) {
+                return false;
+            }else if (this.playerHP <= 50) {
+                this.playerHP += 10
+            }else {
+                this.playerHP = 50;
+            }
+        },
+        lost: function() {
+            this.isStart = false;
+            alert('End game');
+            this.playerHP = this.monsterHP = 100;
+            this.process = [];
         }
     }
 });
